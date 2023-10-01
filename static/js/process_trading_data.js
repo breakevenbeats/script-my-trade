b18a87f5e0e9ee045a7e01974d1d76da83eadb2f
breakevenbeats <breakevenbeats@gmail.com>
2023-09-30 22:18:13 -0700

    Simplify manual user input, make results scrollable

--- static/js/process_trading_data.js
+++ static/js/process_trading_data.js
@@ -8,34 +8,26 @@ var datePicker = new Pikaday({
 document.addEventListener("DOMContentLoaded", function() {
     var form = document.getElementById('fileUploadForm');
     form.addEventListener('submit', function(event) {
-        event.preventDefault(); // Prevent the default form submission behavior
+        event.preventDefault();
 
-        var formData = new FormData(form); // Create a FormData object from the form
+        var formData = new FormData(form);
         fetch('/upload', {
             method: 'POST',
-            body: formData // Use the FormData object as the request body
+            body: formData
         })
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
-            // Update the results div with the response
-            document.querySelector('.result-box ul').innerHTML = data;
+        // Update the results div with the response
+        const ulElement = document.querySelector('.result-box ul');      ulElement.innerHTML = ''; // Clear the existing content if any
+        data.forEach(item => {
+            const liElement = document.createElement('li');
+            liElement.textContent = item;
+                ulElement.appendChild(liElement);
+            });
         })
-        .catch(error => console.error(error));
+        .catch(error => console.error('Error:', error));
     });
 });
-
-function handleInput() {
-	var inputBox = document.getElementById('tsvInput');
-	var placeholder = "Call Buy    09/29/2023 09:54:06 EDT";
-
-	if (inputBox.value === '') {
-		inputBox.style.color = 'black'; // Gray text when empty
-		inputBox.value = placeholder;
-	} else if (inputBox.value === placeholder) {
-		inputBox.style.color = 'black'; // Black text when typing
-		inputBox.value = '';
-	}
-}
 
 function copyToClipboard() {
 	var textToCopy = document.querySelector('.result-box ul').innerText;
@@ -69,22 +61,44 @@ function handleResponse(response) {
 }
 
 function processInput() {
+    var date = document.getElementById('datePicker').value;
     var inputText = document.getElementById('tsvInput').value;
-    if (inputText) {
-        fetch('/process_text', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-            },
-            body: `input_text=${encodeURIComponent(inputText)}`,
-        })
-        .then(handleResponse) // Call handleResponse function
-        .catch(error => console.error(error));
-    } else {
-        alert('Please enter text before submitting.');
-    }
-}
 
+    if (!date || !inputText) {
+        alert('Please select a date and enter text before submitting.');
+        return;
+    }
+
+    // Split the inputText by newline to get each row
+    var inputRows = inputText.split('\n');
+
+    var resultsList = document.getElementById('resultsList');
+    resultsList.innerHTML = ''; // Clear the previous results
+
+    inputRows.forEach(function(row) {
+        try {
+            var [action, type, time] = row.split(' ').map(e => e.toLowerCase());
+
+            if (['buy', 'sell'].indexOf(action) === -1 || ['call', 'put'].indexOf(type) === -1) {
+                alert(`Invalid action or type in row "${row}". Please enter "Buy" or "Sell" for action and "Call" or "Put" for type.`);
+                return;
+            }
+
+            var [month, day, year] = date.split('/');
+            var [hour, minute] = time.split(':');
+
+            var color = action === 'buy' ? 'color.red' : 'color.green';
+            var result = `plotshape(time == timestamp(${year}, ${month}, ${day}, ${hour}, ${minute}), style=shape.triangleup, location=location.belowbar, color=${color}, size=size.small, text='${type.charAt(0).toUpperCase() + type.slice(1)}', textcolor=color.white)`;
+
+            var listItem = document.createElement('li');
+            listItem.textContent = result;
+            resultsList.appendChild(listItem);
+        } catch (error) {
+            console.error(error);
+            alert(`An error occurred while processing the row "${row}".`);
+        }
+    });
+}
 
 function handleTabKey(event) {
 	if (event.key === "Tab") {
@@ -97,3 +111,27 @@ function handleTabKey(event) {
 		textarea.selectionStart = textarea.selectionEnd = start + 1;
 	}
 }
+
+var textarea = document.getElementById('tsvInput');
+var placeholderText = "Buy Call 9:30\nSell Call 9:45\nBuy Put 13:15\nSell Put 13:45";
+
+textarea.value = placeholderText;
+textarea.className = 'placeholderText';
+
+textarea.addEventListener('focus', function() {
+    // If the current value is the placeholder, clear it on focus
+    if (this.value === placeholderText) {
+        this.value = '';
+        this.className = '';
+        this.style.color = '#fff';
+    }
+});
+
+textarea.addEventListener('blur', function() {
+    // If the user didn't enter anything, put the placeholder back on blur
+    if (this.value === '') {
+        this.value = placeholderText;
+        this.className = 'placeholderText';
+        this.style.color = 'lightgray';
+    }
+});
